6303f20f1b40603612854f626ce0e9271a4b13d5
Debby123 <deblina_success@ymail.com>
2014-08-24 14:16:23 +0530

    cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,50 @@
-## Following pair of functions compute and cache the inverse of a matrix
-## so it does not need to be computed repeatedly.
-## This function computes the inverse of the special "matrix" returned by
-## makeCacheMatrix above. If the inverse has already been calculated
-## (and the matrix has not changed), then the cacheSolve should retrieve
-## the inverse from the cache.
-makeCacheMatrix <- function(x = matrix()) {
-s <- NULL
-set <- function(y) {
-x <<- y
-s <<- NULL
+## 'makeCacheMatrix' and 'cacheSolve' are a pair of functions that allow
+## caching the inverse of a matrix to avoid calculating it more than once.
+##
+## Sample usage:
+## m <- matrix(1:4, 2, 2)
+## cm <- makeCacheMatrix(m)
+## inv <- cacheSolve(cm)
+## 'makeCacheMatrix' returns a list that bundles a matrix and its inverse.
+## The matrix can be accessed via '$get()' and changed via '$set()'.
+## The inverse can be accessed via '$getInv()' and changed via '$setInv()'.
+ 
+ makeCacheMatrix <- function(x = matrix())
+ {
+       	 # cached inverse
+	inv <- NULL
+
+	# accessors
+	get <- function() x
+	getInv <- function() inv
+
+	# mutators
+	set <- function(newX) {
+	x <<- newX
+	inv <<- NULL
+ 	}
+
+	setInv <- function(newInv) inv <<- newInv
+	# return the bundle
+	list(get = get, set = set,
+	getInv = getInv, setInv = setInv)
 }
-get <- function() x
-setsolve <- function(solve) s <<- solve
-getsolve <- function() s
-list(set = set, get = get,
-setsolve = setsolve,
-getsolve = getsolve)
-}
-## This function creates a special "matrix" object that can cache its inverse.
-cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
-s <- x$getsolve()
-if(!is.null(s)) {
-message("getting cached data")
-return(s)
-}
-data <- x$get()
-s <- solve(data, ...)
-x$setsolve(s)
-s
+## 'cacheSolve' accepts a matrix bundle created by 'makeCacheMatrix',
+## and returns the inverse of the matrix. It uses the inverse matrix cached
+## in the bundle if already calculated, otherwise it calculates and stores the
+## inverse. Any additional parameters are passed on to 'solve'.
+
+cacheSolve <- function(x, ...)
+ {
+	inv <- x$getInv()
+
+	# invert if necessary
+	if (is.null(inv)) {
+	m <- x$get()
+	inv <- solve(m, ...)
+	x$setInv(inv)
+	}
+
+    	# return the inverse matrix
+	inv
 }
